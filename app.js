90cab35e9dd61ff0da3f012d449134092287054b
KatyaEames <katya.eames@gmail.com>
2015-01-15 11:59:38 -0700

    Reset button

--- app.js
+++ app.js
@@ -8,4 +8,7 @@ $(document).ready(function() {
 	$('.box').on('dblclick', function() {
 		$('this').removeClass('white');
 	})
+	$('#reset').on('click', function() {
+		$('.box').removeClass('white');
+	})
 })
